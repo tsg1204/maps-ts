a4d4a89aefa3c9bda8a2b96979397b239dedd0cd
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-13 13:41:19 -0500

    minor refact of classes finished up

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,8 +1,10 @@
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
-  }
+  };
+  markerContent(): string;
+  color: string;
 }
 
 export class CustomMap {
@@ -28,7 +30,7 @@ export class CustomMap {
     });
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: 'Hi there'
+        content: mappable.markerContent()
       });
 
       infoWindow.open(this.googleMap, marker);
